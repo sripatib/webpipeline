832096bcf758ce645e9b05fac95060b417126d67
sripatib <biswal.sripati7@gmail.com>
2018-05-27 04:42:28 +0530

    3rd commit

--- script/updatehost.sh
+++ script/updatehost.sh
@@ -1,2 +1,2 @@
-echo [web] > /etc/ansible/hosts
-aws  ec2 describe-instances --region us-east-2 --filters "Name=tag:Name,Values=web" --query 'Reservations[].Instances[].[PublicIpAddress]' --output text| grep -v None >> /etc/ansible/hosts
+sudo echo [web] > /etc/ansible/hosts
+sudo aws  ec2 describe-instances --region us-east-2 --filters "Name=tag:Name,Values=web" --query 'Reservations[].Instances[].[PublicIpAddress]' --output text| grep -v None >> /etc/ansible/hosts
