2bf06fb236b083d1f736b4d30b03ad4f9459a16b
sripati <biswal.sripati7@gmail.com>
2018-05-27 04:18:41 +0530

    1st

---
+++ script/updatehost.sh
@@ -0,0 +1,2 @@
+echo [web] > /etc/ansible/hosts
+aws  ec2 describe-instances --region us-east-2 --filters "Name=tag:Name,Values=web" --query 'Reservations[].Instances[].[PublicIpAddress]' --output text| grep -v None >> /etc/ansible/hosts
