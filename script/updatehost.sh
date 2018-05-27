c637894c21b39d12eff4368b49cbb492abcf3faa
sripatib <biswal.sripati7@gmail.com>
2018-05-27 12:50:19 +0530

    [3rd commit]

--- script/updatehost.sh
+++ script/updatehost.sh
@@ -1,4 +1,2 @@
-chmod 664 /etc/ansible/hosts
-chown :jenkins /etc/ansible/hosts
 echo [web] > /etc/ansible/hosts
 aws  ec2 describe-instances --region us-east-2 --filters "Name=tag:Name,Values=web" --query 'Reservations[].Instances[].[PublicIpAddress]' --output text| grep -v None >> /etc/ansible/hosts
